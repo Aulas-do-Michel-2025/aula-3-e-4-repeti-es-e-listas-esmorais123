1d6913c8dbcb0bab25cf0029f01240db77430280
Eric <ericsm123@gmail.com>
2025-04-15 15:29:34 -0300

    Update exercicio_4.py

--- exercicio_4.py
+++ exercicio_4.py
@@ -40,3 +40,25 @@ O cálculo de média já foi feito em sala e pode ser usado de exemplo.
 lista_de_organismos = [[50, 50, 50], [125, 99, 12], [19, 91, 42], [40, 189, 0], [1, 0, 0], [100, 100, 70], [99, 12, 12]]
 
 # Fazer a partir daqui
+medias = []
+somas = []
+
+for i in range(len(lista_de_organismos)):
+    soma = 0
+    for j in range(len(lista_de_organismos[i])):
+        soma += lista_de_organismos[i][j]
+    somas.append(soma)
+
+#print(somas)
+
+for x in somas:
+    media = x / len(lista_de_organismos[i])
+    medias.append(media)
+
+#print(medias)
+
+for y in range(len(medias)):
+    if medias[y] == max(medias):
+        organismo_maior_media = y
+
+print(f'O organismo com maior média é o da posição {organismo_maior_media} da lista.')
