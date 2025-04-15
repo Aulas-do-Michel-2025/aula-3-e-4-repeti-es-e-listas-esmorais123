edde0dac9aca3b6d80c61b4447777a7030d19767
Eric <ericsm123@gmail.com>
2025-04-15 14:03:58 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -21,8 +21,6 @@ Os números ímpares são [1, 3, 5]
 
 # Código para pegar a lista
 lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
-
-lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
 lista_impar = []
 
 for i in range(len(lista)):
